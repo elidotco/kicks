a14ed0d8c2709cafde8aa22e5746d7644df89103
Ebenezer Ametepeh <eddiefestde@gmail.com>
2024-01-24 15:10:09 +0000

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
   return (
     <div className="bg-[#4A69E2] rounded-[48px] relatvie bottom-0">
       <div className=" flex  items-center flex-col md:flex-row gap-28  p-10 ">
-        <div className=" flex flex-col gap-5 w-[40%]">
+        <div className=" flex flex-col gap-5 md:w-[40%]">
           <h3 className="font-semibold leading-none text-[#fff] text-[43px]">
             Join our KicksPlus Club & get 15% off
           </h3>
           <p>Sign up for free! Join the communits</p>
           <div className=" gap-5">
             <input className="bg-transparent h-12  w-[65%] rounded-lg border border-[#fafafa] " />
-            <button className="bg-black text-white px-5 py-3 ml-3 rounded-lg ">
+            <button className="bg-black uppercase text-white px-5 py-3 ml-3 rounded-lg ">
               Submit
             </button>
           </div>
