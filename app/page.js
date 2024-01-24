7f6271cfa699b2730260458b4bf42614bb5e4798
Ebenezer Ametepeh <eddiefestde@gmail.com>
2024-01-24 16:03:41 +0000

    footer changes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Category, Footer, Hero, Review } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  pb-44">
+    <main className="flex min-h-screen flex-col  ">
       <h1 className="text-[50px] text-center font-bold uppercase lg:text-[194px]  w-[auto] ">
         DO it <span className="text-[#4A69E2] "> Right</span>
       </h1>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className="bg-[#4A69E2] rounded-[48px] relatvie bottom-0">
-      <div className=" flex  items-center flex-col md:flex-row gap-28  p-10 ">
-        <div className=" flex flex-col gap-5 md:w-[40%]">
-          <h3 className="font-semibold leading-none text-[#fff] text-[43px]">
+    <div className="bg-[#4A69E2] rounded-3xl md:rounded-[48px] relatvie bottom-0 ">
+      <div className=" flex  md:items-center flex-col md:flex-row gap-10  lg:gap-28 p-4 md:p-10 pb-20">
+        <div className=" flex flex-col gap-5 w-full md:w-[40%]">
+          <h3 className="font-semibold leading-none text-[#fff] text-[32px]      md:text-[43px]">
             Join our KicksPlus Club & get 15% off
           </h3>
           <p>Sign up for free! Join the communits</p>
@@ -19,9 +19,16 @@ const Footer = () => {
           </div>
         </div>
         <div className="">
-          <Image src={logo} alt="lgo image" width={384} height={88} />
+          <Image
+            src={logo}
+            alt="lgo image"
+            width={190}
+            height={47}
+            className="md:w-[384px] md:h-[88px]  "
+          />
         </div>
       </div>
+      <div className="w-full h-96 bg-[#232321] rounded-3xl"></div>
     </div>
   );
 };
