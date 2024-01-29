138df2335d90d7e236044b847d88107775a6e4c1
Ebenezer Ametepeh <eddiefestde@gmail.com>
2024-01-29 07:30:50 +0000

    error fixed

--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
-import { Category, Footer, Hero, Review } from "@/components";
+"use client";
+import { Category, Footer, Hero, Navbar, Review } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  ">
-      <h1 className="text-[50px] text-center font-bold uppercase lg:text-[194px]  w-[auto] ">
-        DO it <span className="text-[#4A69E2] "> Right</span>
-      </h1>
-      <Hero />
-      <Category />
-      <Review />
-      <Footer />
-    </main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col  ">
+        <h1 className="text-[50px] text-center font-bold uppercase lg:text-[194px]  w-[auto] ">
+          DO it <span className="text-[#4A69E2] "> Right</span>
+        </h1>
+        <Hero />
+        <Category />
+        <Review />
+        <Footer />
+      </main>
+    </>
   );
 }
